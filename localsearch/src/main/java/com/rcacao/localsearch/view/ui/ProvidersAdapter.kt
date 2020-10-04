60c6e98d98cef36f3df708ac40ad8817f5b5a239
renan <rcacao10@gmail.com>
2020-10-04 02:27:46 -0300

    adiciona adapter da lista de fornecedores

---
+++ localsearch/src/main/java/com/rcacao/localsearch/view/ui/ProvidersAdapter.kt
@@ -0,0 +1,28 @@
+package com.rcacao.localsearch.view.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rcacao.localsearch.data.model.Provider
+import com.rcacao.localsearch.databinding.ItemProviderBinding
+
+class ProvidersAdapter : RecyclerView.Adapter<ProvidersAdapter.ProviderViewHolder>() {
+
+    var providers: List<Provider> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProviderViewHolder {
+        val binding: ItemProviderBinding =
+            ItemProviderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ProviderViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = providers.size
+
+    override fun onBindViewHolder(holder: ProviderViewHolder, position: Int) {
+        holder.binding.provider = providers[position]
+    }
+
+    class ProviderViewHolder(val binding: ItemProviderBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+}
