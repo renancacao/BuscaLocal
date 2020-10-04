1b2466dd1fdcef4b935d4c4c7ce1213cb3ce5bc7
renan <rcacao10@gmail.com>
2020-10-04 03:21:11 -0300

    Instancia viewmodel na activity via KTX

--- localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
+++ localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
@@ -1,12 +1,16 @@
 package com.rcacao.localsearch.view.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.rcacao.localsearch.R
+import com.rcacao.localsearch.view.viewmodel.ProvidersListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ListSearchActivity : AppCompatActivity() {
+
+    private val viewModel: ProvidersListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
