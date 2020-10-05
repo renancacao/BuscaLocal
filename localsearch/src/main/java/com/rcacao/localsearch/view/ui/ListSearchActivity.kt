ffa80f879811ddf1dbc32b275ca49c04de96a5b4
renan <rcacao10@gmail.com>
2020-10-05 04:24:46 -0300

    inicializa binding na activity de listagem

--- localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
+++ localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
@@ -3,7 +3,10 @@ package com.rcacao.localsearch.view.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.rcacao.localsearch.R
+import com.rcacao.localsearch.databinding.ActivityListSearchBinding
 import com.rcacao.localsearch.view.viewmodel.ProvidersListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -11,9 +14,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class ListSearchActivity : AppCompatActivity() {
 
     private val viewModel: ProvidersListViewModel by viewModels()
+    private val adapter = ProvidersAdapter()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list_search)
+        val binding: ActivityListSearchBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_list_search)
+
+        binding.viewModel = viewModel
+        binding.recyclerView.adapter = adapter
+
+        viewModel.providers.observe(this, Observer {
+            adapter.providers = it
+            adapter.notifyDataSetChanged()
+        })
+
     }
 }
