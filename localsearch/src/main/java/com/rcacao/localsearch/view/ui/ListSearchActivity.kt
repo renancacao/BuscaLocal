f5cc8091faa1287b7498d9e9a31c6ef681d9e3f2
renan <rcacao10@gmail.com>
2020-10-06 00:33:00 -0300

    adiciona tratamento de erro e listener de edição de texto no campo de busca

--- localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
+++ localsearch/src/main/java/com/rcacao/localsearch/view/ui/ListSearchActivity.kt
@@ -1,6 +1,9 @@
 package com.rcacao.localsearch.view.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -11,7 +14,7 @@ import com.rcacao.localsearch.view.viewmodel.ProvidersListViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ListSearchActivity : AppCompatActivity() {
+class ListSearchActivity : AppCompatActivity(), TextWatcher {
 
     private val viewModel: ProvidersListViewModel by viewModels()
     private val adapter = ProvidersAdapter()
@@ -25,11 +28,38 @@ class ListSearchActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
         binding.recyclerView.adapter = adapter
+        binding.textSearch.addTextChangedListener(this)
 
+        observerErrorEvent()
+        observerProvidersList()
+
+    }
+
+    private fun observerProvidersList() {
         viewModel.providers.observe(this, Observer {
-            adapter.providers = it
-            adapter.notifyDataSetChanged()
+            adapter.setProviders(it)
         })
+    }
 
+    private fun observerErrorEvent() {
+        viewModel.error.observe(this, Observer { event ->
+            event.getContentIfNotHandled()?.let { showError(it) }
+        })
+    }
+
+    private fun showError(errorMessage: String) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        //not used
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        //not used
+    }
+
+    override fun onTextChanged(searchText: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        viewModel.search(searchText.toString())
     }
 }
