a26bfc5b8ff38e99bca3a660da08e043ea32c108
renan <rcacao10@gmail.com>
2020-10-06 00:45:11 -0300

    adiciona testes para os UseCases

---
+++ localsearch/src/test/java/com/rcacao/localsearch/domain/usecase/GetProvidersUseCaseImplTest.kt
@@ -0,0 +1,66 @@
+package com.rcacao.localsearch.domain.usecase
+
+import com.nhaarman.mockitokotlin2.MockitoKotlinException
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.rcacao.localsearch.data.model.Provider
+import com.rcacao.localsearch.data.repository.DataResponse
+import com.rcacao.localsearch.data.repository.ProvidersRepository
+import com.rcacao.localsearch.domain.model.GetProvidersResult
+import com.rcacao.localsearch.utils.Constants
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class GetProvidersUseCaseImplTest {
+
+    private lateinit var useCase: GetProvidersUseCase
+    private val repository: ProvidersRepository = mock()
+
+    @Test
+    fun getProvidersOnSuccessReturnProvidersLoaded() {
+        runBlockingTest {
+            val list: List<Provider> = listOf(Provider(), Provider())
+            whenever(repository.getProviders()).thenReturn(DataResponse.Success(list))
+            useCase = GetProvidersUseCaseImpl(repository)
+
+            val result: GetProvidersResult = useCase()
+
+            Assert.assertTrue(result is GetProvidersResult.ProvidersLoaded)
+            Assert.assertEquals(list, (result as GetProvidersResult.ProvidersLoaded).providers)
+        }
+    }
+
+    @Test
+    fun getProvidersOnFailureReturnProvidersError() {
+        runBlockingTest {
+            val ex = MockitoKotlinException("test message", null)
+            whenever(repository.getProviders()).thenReturn(DataResponse.Failure(ex))
+            useCase = GetProvidersUseCaseImpl(repository)
+
+            val result: GetProvidersResult = useCase()
+
+            Assert.assertTrue(result is GetProvidersResult.Error)
+            Assert.assertEquals("test message", (result as GetProvidersResult.Error).errorMessage)
+        }
+    }
+
+    @Test
+    fun getProvidersOnFailureWithoutMessageReturnProvidersErrorGeneric() {
+        runBlockingTest {
+            val ex = MockitoKotlinException(null, null)
+            whenever(repository.getProviders()).thenReturn(DataResponse.Failure(ex))
+            useCase = GetProvidersUseCaseImpl(repository)
+
+            val result: GetProvidersResult = useCase()
+
+            Assert.assertTrue(result is GetProvidersResult.Error)
+            Assert.assertEquals(
+                Constants.GENERIC_ERROR,
+                (result as GetProvidersResult.Error).errorMessage
+            )
+        }
+    }
+}
---
+++ localsearch/src/test/java/com/rcacao/localsearch/domain/usecase/SearchProvidersStarWithCaseSensitiveUseCaseImplTest.kt
@@ -0,0 +1,61 @@
+package com.rcacao.localsearch.domain.usecase
+
+import com.google.gson.Gson
+import com.rcacao.localsearch.data.model.Provider
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class SearchProvidersStarWithCaseSensitiveUseCaseImplTest {
+
+    private lateinit var useCase: SearchProvidersUseCase
+
+    @Test
+    fun searchProvidersOnTextEmptyReturnList() {
+        runBlockingTest {
+            val list: List<Provider> = listOf(Provider(), Provider())
+
+            useCase = SearchProvidersStarWithCaseSensitiveUseCaseImpl()
+            val result: List<Provider> = useCase("", list)
+
+            Assert.assertEquals(list, result)
+        }
+    }
+
+    @Test
+    fun searchProvidersOnNameExistReturnFilteredList() {
+        runBlockingTest {
+            val list: List<Provider> = mockList()
+
+            useCase = SearchProvidersStarWithCaseSensitiveUseCaseImpl()
+            val result: List<Provider> = useCase("Te", list)
+
+            Assert.assertEquals(2, result.size)
+            Assert.assertEquals("Test 1", result[0].name)
+            Assert.assertEquals("Test 2", result[1].name)
+        }
+    }
+
+    @Test
+    fun searchProvidersOnNameNotExistReturnEmptyList() {
+        runBlockingTest {
+            val list: List<Provider> = mockList()
+
+            useCase = SearchProvidersStarWithCaseSensitiveUseCaseImpl()
+            val result: List<Provider> = useCase("Lu", list)
+
+            Assert.assertTrue(result.isEmpty())
+        }
+    }
+
+    private fun mockList(): List<Provider> {
+        val gson = Gson()
+        val provider1: Provider = gson.fromJson("{\"name\":\"Not Test\"}", Provider::class.java)
+        val provider2: Provider = gson.fromJson("{\"name\":\"Test 1\"}", Provider::class.java)
+        val provider3: Provider = gson.fromJson("{\"name\":\"Test 2\"}", Provider::class.java)
+
+        return listOf(provider1, provider2, provider3)
+    }
+}
