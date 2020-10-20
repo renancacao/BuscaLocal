f944ecf29086db684977165090d434d8968f4748
renan <rcacao10@gmail.com>
2020-10-20 11:23:46 -0300

    altera a forma de comparação dos itens da lista

--- localsearch/src/main/java/com/rcacao/localsearch/view/ui/ProviderDiffUtilCallback.kt
+++ localsearch/src/main/java/com/rcacao/localsearch/view/ui/ProviderDiffUtilCallback.kt
@@ -11,7 +11,7 @@ class ProviderDiffUtilCallback(
     override fun getOldListSize(): Int = oldItems.size
     override fun getNewListSize(): Int = newItems.size
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
-        oldItems[oldItemPosition].id == newItems[newItemPosition].id
+        oldItems[oldItemPosition] == newItems[newItemPosition]
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
         oldItems[oldItemPosition].name == newItems[newItemPosition].name
 
